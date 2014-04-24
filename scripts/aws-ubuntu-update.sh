c935137bfdc7583bcd550ffed0693ee4234336e3
unknown <kern@Imitator.virtual7.local>
2014-04-24 13:08:17 +0200

    Changed Logdir

--- scripts/aws-ubuntu-update.sh
+++ scripts/aws-ubuntu-update.sh
@@ -3,6 +3,10 @@
 # Exit when any of these commands fail
 set -e
 
+mkdir -p /opt/software
+chmod 755 /opt/software
+cp /tmp/software/jdk-7u51-linux-x64.tar.gz /opt/software
+
 # Add add-apt repository
 apt-get -y install python-software-properties
 add-apt-repository -y ppa:chris-lea/node.js
